e64214cba511705efbdcc598a9b7b0109c572d42
Gitau Karugi <gkarugi@gmail.com>
2020-11-09 17:34:52 +0300

    fix js issues

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -9,13 +9,13 @@
         <meta name="description" content="{{ $page->description }}">
         <title>{{ $page->title }}</title>
         <link rel="stylesheet" href="{{ mix('css/all.css', 'assets/build') }}">
-        <script src="{{ mix('js/modernizr-2.6.2.min.js', 'assets/build') }}"></script>
+        <script src="assets/build/js/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
         @yield('body')
 
-        <script src="{{ mix('js/jquery-3.0.0.min.js', 'assets/build') }} "></script>
-        <script src="{{ mix('js/bootstrap.min.js', 'assets/build') }} "></script>
-        <script src="{{ mix('js/main.js', 'assets/build') }} "></script>
+        <script src="assets/build/js/jquery-3.0.0.min.js"></script>
+        <script src="assets/build/js/bootstrap.min.js"></script>
+        <script src="assets/build/js/main.js"></script>
     </body>
 </html>
