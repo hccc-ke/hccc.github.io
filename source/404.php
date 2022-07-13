ab9e449b7f4bbcf3fed8bd3b314f945b0f83d309
Gitau karugi <gkarugi@gmail.com>
2022-07-13 13:01:23 +0300

    add 404 page

---
+++ source/404.php
@@ -0,0 +1,13 @@
+@extends('_layouts.page')
+
+@section('page')
+<section class="ls section_padding_50 columns_padding_25">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h2 class="section_header text-center">404</h2>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
